1c28b09151c4b051ccb7edc1812c6d8906207c1e
saurabh12singh <61622700+saurabh12singh@users.noreply.github.com>
2022-07-18 09:42:02 +0530

    Update MutualFund.java

--- src/main/java/com/cognizant/portfolio_management/DailyMutualFundNAV/model/MutualFund.java
+++ src/main/java/com/cognizant/portfolio_management/DailyMutualFundNAV/model/MutualFund.java
@@ -4,9 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-/*
- * This JPA model class is used to handle database
- */
+
 @Entity
 @Table
 public class MutualFund {
